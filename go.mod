9695a15109006853940243d54f0fb283fe961316
Smotrov Dmitriy <dsxack@gmail.com>
2019-02-09 04:15:49 +0300

    added EncodeJSONResponse

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
 	github.com/stretchr/testify v1.2.2
 )
--- kit/transport/http/encode_decode.go
+++ kit/transport/http/encode_decode.go
@@ -3,7 +3,9 @@ package http
 import (
 	"context"
 	"encoding/json"
+	kitendpoint "github.com/go-kit/kit/endpoint"
 	kithttp "github.com/go-kit/kit/transport/http"
+	"io"
 	"net/http"
 	"reflect"
 )
@@ -19,4 +21,44 @@ func MakeJSONRequestDecoder(requestFactory func() interface{}) kithttp.DecodeReq
 		refReq := reflect.ValueOf(request)
 		return refReq.Elem().Interface(), nil
 	}
+}
+
+// RawResponder is checked by EncodeJSONResponse. It should return io.Reader to copy it to http.ResponseWriter
+type RawResponder interface {
+	RawResponse() io.Reader
+}
+
+// EncodeJSONResponse is a EncodeResponseFunc that serializes the response as a
+// JSON object to the ResponseWriter. Its works like standard go-lit EncodeJSONResponse but
+// checks Failer and RawResponder interfaces
+func EncodeJSONResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	if failer, ok := response.(kitendpoint.Failer); ok && failer.Failed() != nil {
+		kithttp.DefaultErrorEncoder(ctx, failer.Failed(), w)
+		return nil
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	if headerer, ok := response.(kithttp.Headerer); ok {
+		for k, values := range headerer.Headers() {
+			for _, v := range values {
+				w.Header().Add(k, v)
+			}
+		}
+	}
+
+	code := http.StatusOK
+	if sc, ok := response.(kithttp.StatusCoder); ok {
+		code = sc.StatusCode()
+	}
+	w.WriteHeader(code)
+	if code == http.StatusNoContent {
+		return nil
+	}
+
+	if rawResponse, ok := response.(RawResponder); ok {
+		_, err := io.Copy(w, rawResponse.RawResponse())
+		return err
+	}
+
+	return json.NewEncoder(w).Encode(response)
 }
